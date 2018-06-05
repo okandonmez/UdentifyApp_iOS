ef906fbf604f6be6ad526b698adc2647a96ea1bd
Udentify <apple@apple-macbook-air.local>
2018-06-05 17:31:09 +0300

    Initial Commit

---
+++ UdentifyApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UdentifyApp
+//
+//  Created by Apple on 5.06.2018.
+//  Copyright © 2018 Udentify. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
